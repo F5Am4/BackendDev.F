526c8f03a791703f6ea480f36210f3be9ebe613a
zonicness <zonicness@hotmail.com>
2023-09-08 20:30:49 -0600

    ejemplos de endpoints

--- 06-express/index.js
+++ 06-express/index.js
@@ -15,14 +15,47 @@ import express from 'express'
 // inicializamos el modulo y lo guardamos en una variable (app) 
 const app = express()
 
-app.get('/', function(req, res){
-    console.log('HOLA MUNDO')
+app.use(express.json())
+
+// Endpoint por default
+app.get('/', function(request, response){
+    console.log(request.query)
+    response.status(200).send({mensaje: 'hola mundo'})
 })
 
+// Endpoint de users 'Get' (ejemplo)
+app.get('/users', function(request, response){
+    const users = ['Camila', 'Felipe', 'Jason']
+    response.status(200).send({data: users})
+})
 
+// Endpoint de users 'Get con params' (ejemplo)
+app.get('/users/:user_id', function(request, response){
+    console.log(request.params.user_id)
+    response.status(200).send({mensaje: 'usuario encontrado'})
+})
+
+// Endpoint de users 'Post' (ejemplo)
+app.post('/users', function(request, response){
+    if(!request.body.name) {
+        response.status(400).send({error: 'Por favor envía los datos del usuario'})
+    }
+
+    const userName = request.body.name
+    console.log(userName)
+
+    response.status(201).send({mensaje: 'Usuario creado'})
+})
+
+// Endpoint de cars 'Post' (ejemplo)
+app.post('/cars', function(request, response){
+    console.log(request.body.brand)
+    response.status(200).send({mensaje: 'Carro creado'})
+})
 
 // utilizamos la función listen del modulo para correr el servidor
+// Esta función siempre debe estar al final
 app.listen(3000, ()=> {
     console.log('server running')
 })
-// clase min 01:12:50 16/08/2023
+// clase min 01:37:44 16/08/2023
