d2db6bf5584f1be882fb446cc5594e230e5edf0b
zonicness <zonicness@hotmail.com>
2023-09-13 18:04:25 -0600

    se creó DAO para fruta

--- 07-backend+SQL/7.1-frutas/index.js
+++ 07-backend+SQL/7.1-frutas/index.js
@@ -7,4 +7,3 @@ app.use(express.json());
 app.use(router)// se conectan las rutas que estan en la carpeta routes
 
 app.listen(8080, () => console.log('Server listening on portr 8080'))
-// sigue clase del 23/08/2023 clase sig. desde el 11/09/23
